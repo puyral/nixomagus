df5dc947ffa5628236bedd2f28a6927aca11cf7c
Puyral <7871851+puyral@users.noreply.github.com>
2024-02-10 17:48:24 +0100

    up

--- users/simon/custom-packages.nix
+++ users/simon/custom-packages.nix
@@ -1,8 +1,6 @@
 { system, ... }: {
   home.packages = [
-    # import (builtins.getFlake "github:puyral/clocktui?rev=e9839a439a4ff123b6acf629f198eea3f6a69973").packages.x86_64-linux.default
     (builtins.getFlake
-      "github:puyral/clocktui?rev=e9839a439a4ff123b6acf629f198eea3f6a69973").packages.${system}.default
-    # (builtins.getFlake "path:/tmp/clocktui").packages.${system}.default
+      "github:puyral/clocktui?rev=950d9f909995553cdebfc35519a91c64189da3a2").packages.${system}.default
   ];
 }
