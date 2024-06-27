fbacfc430c060c7870d56a931d25680d13c696b0
Puyral <7871851+puyral@users.noreply.github.com>
2024-06-27 22:45:59 +0200

    update and kmonand

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 
     kmonad = {
       url = "github:kmonad/kmonad?dir=nix";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     custom = {
--- kmonad/kmonad.nix
+++ kmonad/kmonad.nix
@@ -6,7 +6,7 @@
 
   services.kmonad = {
     enable = true;
-    package = pkgs.haskellPackages.kmonad;
+    package = pkgs.kmonad;
     keyboards = {
       hailuck = {
         device = "/dev/input/by-id/usb-HAILUCK_CO._LTD_USB_KEYBOARD-event-kbd";
