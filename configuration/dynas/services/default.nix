3d4539e29ed20893b34ede058ed2d3f714c8188b
Puyral (dynas) <7871851+puyral@users.noreply.github.com>
2024-09-15 23:21:39 +0200

    jellyfin

--- configuration/dynas/services/default.nix
+++ configuration/dynas/services/default.nix
@@ -5,5 +5,15 @@
     ./samba.nix
     ./cockpit.nix
     ./syncthing.nix
+    ./whatchtower.nix
+    ./jellyfin.nix
   ];
+
+  #   networking.nat = {
+  #   enable = true;
+  #   internalInterfaces = ["ve-+"];
+  #   externalInterface = "enp9s0";
+  #   # Lazy IPv6 connectivity for the container
+  #   enableIPv6 = true;
+  # };
 }
---
+++ configuration/dynas/services/jellyfin.nix
@@ -0,0 +1,49 @@
+{ mconfig, ... }:
+let
+  base_dir = "/containers/jellyfin";
+in
+{
+  containers.jellyfin = {
+    bindMounts = {
+      "/var/data/jellyfin" = {
+        hostPath = "${base_dir}/data";
+        isReadOnly = false;
+      };
+      "/var/cache/jellyfin" = {
+        hostPath = "${base_dir}/cache";
+        isReadOnly = false;
+      };
+      "/mnt/Zeno/media/videos" = {
+        hostPath = "/mnt/Zeno/media/videos";
+        isReadOnly = false;
+      };
+    };
+    autoStart = true;
+
+    config =
+      { lib, ... }:
+      {
+        services.jellyfin = {
+          enable = true;
+          openFirewall = true;
+        };
+        services.jellyseerr.enable = true;
+
+        system.stateVersion = mconfig.nixos;
+        networking = {
+          firewall.enable = true;
+          # Use systemd-resolved inside the container
+          # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
+          useHostResolvConf = lib.mkForce false;
+        };
+
+        programs.nix-ld.enable = true;
+        services.resolved.enable = true;
+      };
+  };
+  networking.firewall = {
+    allowedTCPPorts = [ 8096 8920 ];
+      allowedUDPPorts = [ 1900 7359 ];
+  };
+
+}
