c67d9f019dc03e747191a7119e39bb09bd574c4f
Puyral (dynas) <7871851+puyral@users.noreply.github.com>
2024-09-30 19:38:28 +0200

    ground work for vaultwarden

---
+++ modules/nixos/vaultwarden/default.nix
@@ -0,0 +1,42 @@
+{lib,config,  ...}:
+let cfg = config.exact.vaultwarden; 
+name = "vaultwarden";
+port = 8222;
+in
+{
+    options.extra.vaultwarden = with lib; {
+    enable = mkEnableOption "vaultwarden";
+    dongle = mkOption { type = types.path; };
+    dataDir = mkOption {
+      type = types.path;
+      default = "/containers/vaultwarden";
+    };
+    config = mkIf cfg.enable {
+      containers.${name} = {
+        bindMounts = {
+          "/var/lib/bitwarden_rs" = {
+            hostPath = "${cfg.dataDir}/state";
+            isReadOnly = false;
+          };
+          "/backup" = {
+            hostPath = "${cfg.dataDir}/backup";
+            isReadOnly = false;
+          };
+        };
+        autoStart = true;
+        ephemeral = true;
+
+        config = {...}:{
+          services.vaultwarden = {
+            enable = true;
+            config = {
+              ROCKET_ADDRESS = "0.0.0.0";
+              ROCKET_PORT = port;
+            };
+            backupDir = "/backup";
+          };
+        };
+      };
+    };
+  };
+}
