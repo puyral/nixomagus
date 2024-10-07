0d46082c4691922fc5279840e8c2e04c485edf9b
Puyral (mydos)] <7871851+puyral@users.noreply.github.com>
2024-10-07 23:23:57 +0200

    tmpfs on mydos

--- configuration/mydos/hardware-configuration.nix
+++ configuration/mydos/hardware-configuration.nix
@@ -46,6 +46,9 @@
 
   swapDevices = [ { device = "/swap/swapfile"; } ];
 
+  boot.tmp.useTmpfs = true;
+  boot.tmp.tmpfsSize = "50%";
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
