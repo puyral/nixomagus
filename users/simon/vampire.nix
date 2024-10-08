6e0878645ea0a7cad3c5192876c3511b3e4a0854
puyral <7871851+puyral@users.noreply.github.com>
2024-10-08 19:29:23 +0200

    up

--- users/simon/vampire.nix
+++ users/simon/vampire.nix
@@ -4,6 +4,9 @@
   home = {
     packages = (with custom; [ vampire-master ]) ++ (with pkgs-unstable; [ elan ]);
   };
-  services.gpg-agent.enable = true;
-  programs.gnupg.agent.pinentryPackage =  pkgs.pinentry-curses;
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = "ncurse";
+  };
+  # programs.gnupg.agent.pinentryPackage =  pkgs.pinentry-curses;
 }
