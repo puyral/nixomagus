fe0ede0263b8b42f35c38573958e3cd0a31817d1
Puyral (mydos)] <7871851+puyral@users.noreply.github.com>
2024-11-28 14:19:10 +0100

    tweaked registeries

--- registeries.nix
+++ registeries.nix
@@ -1,10 +1,11 @@
-{ nixpkgs-unstable, ... }:
+{ nixpkgs-unstable, nixpkgs, ... }:
 {
   nix.registry = {
     # because of https://github.com/NixOS/nixpkgs/commit/e456032addae76701eb17e6c03fc515fd78ad74f I can't remap `nixpkgs` itself
     gnixpkgs.flake = nixpkgs-unstable // {
       config.allowUnfree = true;
     };
+    stable.flake = nixpkgs // { config.allowUnfree = true; };
     latest-unstable = {
       from = {
         type = "indirect";
