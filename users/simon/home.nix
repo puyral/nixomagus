3cf940a27166eeda046f4b47287a4146388dbd32
puyral <7871851+puyral@users.noreply.github.com>
2024-12-26 13:47:58 +0100

    fix env var

--- users/simon/home.nix
+++ users/simon/home.nix
@@ -62,7 +62,7 @@
     #
     sessionVariables = {
       EDITOR = "vim";
-      CONFIG_LOCATION = if is_nixos then "/config" else "~/.config/home-manager/";
+      CONFIG_LOCATION = if is_nixos then "/config" else "$HOME/.config/home-manager/";
     };
   };
 
