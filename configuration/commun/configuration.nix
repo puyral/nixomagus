f45ce4f1dd6d0220a1025745aa7842551cf59b6c
puyral <7871851+puyral@users.noreply.github.com>
2025-02-08 10:50:42 +0100

    meh

--- configuration/commun/configuration.nix
+++ configuration/commun/configuration.nix
@@ -28,6 +28,7 @@
   programs.gnupg = {
     agent = {
       enable = true;
+      enable-ssh-support = true;
       # pinentryPackage = pkgs.pinentry-gnome3;
       # pinentryFlavor = "gnome3";
     };
@@ -147,10 +148,6 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   # List services that you want to enable:
 
