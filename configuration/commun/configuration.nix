9a24b8445e88e4f483d8283502065d61e6f46886
puyral <7871851+puyral@users.noreply.github.com>
2025-03-10 20:53:42 +0100

    add cachix from the nix comunity

--- configuration/commun/configuration.nix
+++ configuration/commun/configuration.nix
@@ -190,6 +190,16 @@
     }
   ];
 
+  nix.settings = {
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
