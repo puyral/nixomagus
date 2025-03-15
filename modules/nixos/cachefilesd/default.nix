0dcf0d3422ff8cbea16d72151d8be86f3749e7fc
puyral <7871851+puyral@users.noreply.github.com>
2025-03-15 23:43:00 +0100

    fix cachefilesd

--- modules/nixos/cachefilesd/default.nix
+++ modules/nixos/cachefilesd/default.nix
@@ -15,18 +15,18 @@ in
   config.services.cachefilesd = lib.mkIf cfg.enable {
     enable = true;
     cacheDir = cfg.cacheDir;
-    extraConfig = ''
-      # Taille maximale du cache
-      brun 20%
-      bcull 15%
+    # extraConfig = ''
+    #   # Taille maximale du cache
+    #   brun 20%
+    #   bcull 15%
 
-      # Paramètres pour l'écriture différée
-      wrun 10%
-      wcull 5%
+    #   # Paramètres pour l'écriture différée
+    #   wrun 10%
+    #   wcull 5%
 
-      # Délai de validation (non directement supporté, ajustez d'autres paramètres)
-      # timeout n'est pas un paramètre standard de cachefilesd
-    '';
+    #   # Délai de validation (non directement supporté, ajustez d'autres paramètres)
+    #   # timeout n'est pas un paramètre standard de cachefilesd
+    # '';
 
   };
 }
