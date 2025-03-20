9382611e3e59854bcc8f42d17763ab30c0e899b1
Puyral (dynas) <7871851+puyral@users.noreply.github.com>
2025-03-20 22:19:54 +0100

    unlock git crypt x4

--- configuration/dynas/services/github/git-crypt-unlocker.nix
+++ configuration/dynas/services/github/git-crypt-unlocker.nix
@@ -20,13 +20,13 @@ writeShellApplication {
   text = ''
     echo "$GPG_PRIVATE_KEY" | base64 -d > /tmp/git-crypt-key.asc
 
-    gpg --batch --import /tmp/git-crypt-key.asc
-
     gpgconf --kill gpg-agent
 
     gpg-agent --daemon --allow-preset-passphrase --max-cache-ttl 3153600000
 
     ${gnupg}/libexec/gpg-preset-passphrase --preset --passphrase "$GPG_KEY_PASS" "$GPG_KEY_GRIP"
+
+    gpg --batch --import /tmp/git-crypt-key.asc
 
     git-crypt unlock
 
