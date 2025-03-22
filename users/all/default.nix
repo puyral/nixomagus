bb0ea092ce9cc8147c070af8367e9eff7404ce84
puyral <7871851+puyral@users.noreply.github.com>
2025-03-22 23:51:44 +0100

    auto build vampire + hm in vampire

--- users/all/default.nix
+++ users/all/default.nix
@@ -1,7 +1,19 @@
-{ rootDir, ... }:
+{
+  rootDir,
+  lib,
+  is_nixos,
+  home-manager,
+  pkgs,
+  ...
+}:
 {
   imports = [ (rootDir + "/registeries.nix") ];
   nixpkgs.config = {
     allowUnfree = _: true;
+  };
+  home = lib.mkIf (!is_nixos) {
+    packages = [
+      home-manager.packages.${pkgs.system}.default
+    ];
   };
 }
