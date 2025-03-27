ee44c2d7cdc93e489e6fb4095952b3b11f41039e
Puyral (dynas) <7871851+puyral@users.noreply.github.com>
2025-03-27 14:33:33 +0100

    Revert "saving dynas x1"
    This reverts commit c6aa26c6a6ee2363a3c9e426f43da828244afb39.

--- configuration/dynas/filesystem.nix
+++ configuration/dynas/filesystem.nix
@@ -2,62 +2,34 @@
 {
   imports = [
     ./zfs.nix
-    # ../commun/filesystem.nix
+    ../commun/filesystem.nix
   ];
-  fileSystems =
-    let
-      pool = "DynasRoot";
-      mkZfs = name: {
-        device = "${pool}/${name}";
-        fsType = "zfs";
-      };
-
-      fs =
-        mounts:
-        with builtins;
-        listToAttrs (
-          map (name: {
-            inherit name;
-            value = mkZfs name;
-          }) mounts
-        );
-
-    in
-    (
-      {
-        "/boot" = {
-          device = "/dev/disk/by-label/NIXBOOT";
-          fsType = "vfat";
-          options = [
-            "fmask=0022"
-            "dmask=0022"
-          ];
-        };
-        "/" = {
-          device = pool;
-          fsType = "zfs";
-        };
-      }
-      // fs [
-        "/nix"
-        "/home"
-        "/var"
-        "/containers"
-        "/config"
-      ]
-    );
-
-  boot = {
-    supportedFilesystems = [ "zfs" ];
-    zfs = {
-      extraPools = [ "DynasRoot" ];
-      forceImportRoot = true; # whatever you, do boot
-
-    };
-    tmp = {
-      useTmpfs = true;
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-uuid/624A-A8EF";
+      fsType = "vfat";
+      options = [
+        "fmask=0022"
+        "dmask=0022"
+      ];
     };
 
+    "/containers" = {
+      device = "/dev/disk/by-label/NIXROOT";
+      fsType = "btrfs";
+      options = [
+        "subvol=containers"
+        "compress=zstd"
+      ];
+    };
+    "/swap" = {
+      label = "NIXROOT";
+      fsType = "btrfs";
+      options = [
+        "subvol=swap"
+        "noatime"
+      ];
+    };
   };
-  swapDevices = [ ];
+  swapDevices = [ { device = "/swap/swapfile"; } ];
 }
