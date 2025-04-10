8c72279b5738e388721f64ce7d63f0ccd186bb78
puyral <7871851+puyral@users.noreply.github.com>
2025-04-10 16:18:52 +0200

    devshell rnote

--- devShells/rnote.nix
+++ devShells/rnote.nix
@@ -4,14 +4,28 @@
   custom,
   cargo-expand,
   rust-analyzer,
+  cargo,
+  cargo-nextest,
+  libadwaita,
+  wrapGAppsHook,
+  pkgs,
   ...
 }:
 mkShell {
   name = "rnote";
-  buildInputs = custom.rnote.buildInputs ++ [
-    cargo-expand
-    rust-analyzer
-    rustPlatform.bindgenHook
-    rustPlatform.cargoSetupHook
-  ];
+  buildInputs =
+    custom.rnote.buildInputs
+    ++ custom.rnote.nativeBuildInputs
+    ++ [
+      cargo
+      cargo-expand
+      rust-analyzer
+      cargo-nextest
+      wrapGAppsHook
+
+      libadwaita
+    ];
+  shellHook = ''
+    export GSETTINGS_SCHEMA_DIR=${pkgs.glib.getSchemaPath pkgs.gtk4}
+  '';
 }
