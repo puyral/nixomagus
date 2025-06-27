8ef7b39e8ba3e0be3613036881539c794671619a
puyral <7871851+puyral@users.noreply.github.com>
2025-06-27 10:53:13 +0200

    update ssh ips

--- modules/home-manager/ssh/default.nix
+++ modules/home-manager/ssh/default.nix
@@ -27,31 +27,14 @@ in
 
         programs.ssh =
           let
-            vampire-ip = "100.111.36.99";
-            nixomagus-ip = "100.106.190.9";
-            amdra-ip = "100.84.31.85";
+            ips = import ./secrets/ip.nix;
           in
           {
             enable = true;
-            matchBlocks = {
-              dynas = {
-                hostname = "dynas.puyral.fr";
-              };
-              vampire = {
-                hostname = vampire-ip;
-              };
+            matchBlocks = (lib.mapAttrs (_: ip: { hostname = ip; }) ips) // {
               "vampire-root" = {
-                hostname = vampire-ip;
+                hostname = ips.vampire;
                 user = "root";
-              };
-              nixomagus = {
-                hostname = nixomagus-ip;
-              };
-              amdra = {
-                hostname = amdra-ip;
-              };
-              ovh-pl = {
-                hostname = "ovh-pl.puyral.fr";
               };
             };
           };
