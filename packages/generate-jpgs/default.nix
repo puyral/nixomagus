8691b2d519856f7e68b2a302af1a425b15ad35a7
Puyral (dynas) <7871851+puyral@users.noreply.github.com>
2025-08-04 12:03:02 +0200

    recompile darktable with gcc instead of clang

--- packages/generate-jpgs/default.nix
+++ packages/generate-jpgs/default.nix
@@ -4,19 +4,17 @@
   writeText,
   darktable,
   python312,
-  clangStdenv,
   gen-config ? null,
   ...
 }:
 let
   configFile =
     if gen-config == null then "" else writeText "config.json" (builtins.toJSON gen-config);
-  mdarktable = darktable.override { stdenv = clangStdenv; };
 in
 # configFile = writeText "";
 writeShellApplication {
   name = "generate-jpgs";
-  runtimeInputs = [ mdarktable ];
+  runtimeInputs = [ darktable python312];
   text = ''
     ${python312}/bin/python ${./generate-jpgs.py} ${configFile}
   '';
