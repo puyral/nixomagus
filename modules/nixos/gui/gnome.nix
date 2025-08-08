7086164fb49b8b60f5ac83e2ca70c1eaeeebe0ae
puyral <7871851+puyral@users.noreply.github.com>
2025-08-08 10:01:43 +0200

    disable suspend on gdm

--- modules/nixos/gui/gnome.nix
+++ modules/nixos/gui/gnome.nix
@@ -13,8 +13,10 @@ in
   config = lib.mkIf (cfg.enable && cfg.gnome) {
     services.xserver.enable = true;
     services.desktopManager.gnome.enable = true;
-    services.displayManager.gdm.enable = true;
-
+    services.displayManager.gdm = {
+      enable = true;
+      autoSuspend = false;
+    };
     environment.systemPackages = with pkgs.gnomeExtensions; [
       wallpaper-slideshow
       tailscale-status
