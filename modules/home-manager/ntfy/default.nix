eaf7778d3a6a90e4f204018b2c33a976ca8299dc
Puyral (dynas) <7871851+puyral@users.noreply.github.com>
2026-01-15 15:10:32 +0100

    ntfy

--- modules/home-manager/ntfy/default.nix
+++ modules/home-manager/ntfy/default.nix
@@ -15,37 +15,22 @@ in
       default = "https://ntfy.puyral.fr";
       description = "The ntfy server URL";
     };
-    topic = lib.mkOption {
-      type = lib.types.str;
-      default = "simon";
-      description = "Default topic to publish to";
-    };
   };
 
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.ntfy-sh ];
 
     sops.secrets.ntfy_token = {
-      sopsFile = ../../../users/simon/secrets-sops/ntfy.yaml;
+      sopsFile = ./ntfy.sops-secret.yaml;
       format = "yaml";
     };
-
-    # Use the user's SSH keys for decryption (RSA or ECDSA)
-    sops.age.sshKeyPaths = [
-      "${config.home.homeDirectory}/.ssh/id_rsa"
-      "${config.home.homeDirectory}/.ssh/id_ecdsa"
-    ];
 
     sops.templates."client.yml" = {
       content = ''
         default-host: ${cfg.url}
-        default-access-token: ${config.sops.placeholder.ntfy_token}
+        default-token: ${config.sops.placeholder.ntfy_token}
       '';
       path = "${config.xdg.configHome}/ntfy/client.yml";
-    };
-
-    programs.zsh.shellAliases = {
-      notify = "ntfy publish ${cfg.topic}";
     };
   };
 }
