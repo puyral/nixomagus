52a539eb1f7a4915cb144183d95b6ddb4c893afd
Puyral (dynas) <7871851+puyral@users.noreply.github.com>
2026-01-31 11:11:04 +0100

    updateing mail-server

--- modules/nixos/mail-server/default.nix
+++ modules/nixos/mail-server/default.nix
@@ -4,24 +4,14 @@ let
   domain = cfg.domain;
 in
 {
+  imports = [ ./options.nix ];
+
   # mostly pulled straight out of https://nixos-mailserver.readthedocs.io/en/latest/setup-guide.html
 
   config = lib.mkIf cfg.enable {
-    security.acme = {
-      acceptTerms = true;
-      defaults.email = "security@${domain}";
-      certs.${config.mailserver.fqdn} = {
-        # I'd like to reuse somwhat what I already have
-        # see the traefik configuration
-
-        # Further setup required, check the manual:
-        # https://nixos.org/manual/nixos/stable/#module-security-acme
-      };
-    };
-
     sops.secrets.mail-passwd = {
       sopsFile = ./passwords.sops-secret.json;
-      type = "json";
+      format = "json";
       key = "simon";
     };
 
@@ -31,15 +21,18 @@ in
       fqdn = "mail.${domain}";
       domains = [ domain ];
 
-      # ???
-      x509.useACMEHost = config.mailserver.fqdn;
+      certificateScheme = "acme";
+      acmeCertificateName = domain;
 
       # A list of all login accounts. To create the password hashes, use
       # nix-shell -p mkpasswd --run 'mkpasswd -s'
       loginAccounts = {
         "simon@${domain}" = {
           hashedPasswordFile = config.sops.secrets.mail-passwd.path;
-          aliases = [ "postmaster@${domain}" "security@${domain}" ];
+          aliases = [
+            "postmaster@${domain}"
+            "security@${domain}"
+          ];
         };
       };
     };
