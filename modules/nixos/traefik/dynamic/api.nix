fe9f22303253fb561424ec5e07883cda247cf217
Puyral (dynas) <7871851+puyral@users.noreply.github.com>
2026-02-17 16:23:03 +0100

    traefik allow for port 80 on demand
    and headscale uses it

--- modules/nixos/traefik/dynamic/api.nix
+++ modules/nixos/traefik/dynamic/api.nix
@@ -5,11 +5,20 @@ in
 {
   services.traefik.dynamicConfigOptions = {
     http = {
-      routers.dashboard = {
-        rule = "Host(`traefik.${name}.puyral.fr`)";
-        service = "api@internal";
-        middlewares = [ "auth" ];
-        tls.certResolver = "ovh";
+      routers = {
+        dashboard = {
+          rule = "Host(`traefik.${name}.puyral.fr`)";
+          service = "api@internal";
+          middlewares = [ "auth" ];
+          entryPoints = [ "https" ];
+          tls.certResolver = "ovh";
+        };
+        dashboard-insecure = {
+          rule = "Host(`traefik.${name}.puyral.fr`)";
+          entryPoints = [ "http" ];
+          middlewares = [ "redirect-to-https" ];
+          service = "api@internal";
+        };
       };
       middlewares.auth.basicAuth.users = [
         "admin:$2y$05$LBy0ZJiKSvCiL0cOqIZgFu0ktqkewdKN6MVIIz3WX2OXsDkHD7Q02"
