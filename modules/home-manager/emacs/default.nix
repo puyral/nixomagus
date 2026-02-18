fad8c83ea3352c13712f73199fe3812b5fbfda7e
puyral <7871851+puyral@users.noreply.github.com>
2026-02-18 16:17:47 +0100

    rebuild Wed Feb 18 04:17:47 PM CET 2026

--- modules/home-manager/emacs/default.nix
+++ modules/home-manager/emacs/default.nix
@@ -8,19 +8,18 @@
 }:
 let
   cfg = config.extra.emacs;
-  
+
   squirrel-mode-epkgs = pkgs.emacsPackages.trivialBuild rec {
     pname = "squirrel-mode";
     version = "unstable";
     src = squirrel-prover-src;
     packageRequires = [ pkgs.emacsPackages.proof-general ];
     allowCompilation = false;
-buildCommand = ''
+    buildCommand = ''
        mkdir  -p $out/share/emacs/site-lisp
           echo "(message \"========== SQUIRREL.EL LOADED FROM NIX STORE ==========\")" > $out/share/emacs/site-lisp/squirrel.el
-          cat ${src}/utils/squirrel.el >> $out/share/emacs/site-lisp/squirrel.el
-          cp ${src}/utils/squirrel-syntax.el $out/share/emacs/site-lisp/
           echo "" >> $out/share/emacs/site-lisp/squirrel.el
+          echo ";;; Define required variables for proof-easy-config" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-toolbar-entries nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-menu-entries nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-prog-args nil)" >> $out/share/emacs/site-lisp/squirrel.el
@@ -32,20 +31,24 @@ buildCommand = ''
           echo "(defvar squirrel-help-menu-entries nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-toolbar-buttons nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-menu-automation nil)" >> $out/share/emacs/site-lisp/squirrel.el
-echo "(defvar squirrel-prog-env nil)" >> $out/share/emacs/site-lisp/squirrel.el
+          echo "(defvar squirrel-prog-env nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-process-regexp nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-prog-args-quiet nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-main-menu-entries nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-goal-custom nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-response-start-regexp nil)" >> $out/share/emacs/site-lisp/squirrel.el
-          echo "(defvar squirrel-response-end-regexp nil)" >> $out/share/emacs/site-lisp/squirrel.el
+echo "(defvar squirrel-response-end-regexp nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-one-command-per-line t)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-version \"custom\")" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-marker-regexp nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-prog-contents-list nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar squirrel-newline-command nil)" >> $out/share/emacs/site-lisp/squirrel.el
           echo "(defvar proof-marker-regexp nil)" >> $out/share/emacs/site-lisp/squirrel.el
-         '';
+          echo "" >> $out/share/emacs/site-lisp/squirrel.el
+          echo "; Copy original squirrel.el content" >> $out/share/emacs/site-lisp/squirrel.el
+          cat ${src}/utils/squirrel.el >> $out/share/emacs/site-lisp/squirrel.el
+          cp ${src}/utils/squirrel-syntax.el $out/share/emacs/site-lisp/
+        '';
   };
 in
 {
@@ -64,4 +67,3 @@ in
     home.packages = [ ] ++ lib.optional cfg.squirrel.enable pkgs-self.squirrel;
   };
 }
- 
