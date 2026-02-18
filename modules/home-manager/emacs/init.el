6b91db5aae4bccbc52b8f57e0de03235c02bcf00
puyral <7871851+puyral@users.noreply.github.com>
2026-02-18 17:02:28 +0100

    .

--- modules/home-manager/emacs/init.el
+++ modules/home-manager/emacs/init.el
@@ -1,7 +1,19 @@
+(message "========== LOADING CUSTOM INIT.EL ==========")
+
+;; Define required variables for squirrel mode before loading proof-site
+;; (defvar squirrel-prog-args nil "Arguments for squirrel prover")
+;; (defvar squirrel-toolbar-entries nil "Toolbar entries for squirrel mode")
+
+;; Register Squirrel with Proof General before loading proof-site
+;; (add-to-list 'proof-assistant-table-default '(squirrel "Squirrel" "sp"))
+
 ;; Load ProofGeneral
-(require 'proof-site)
+;; (require 'proof-site)
 
 ;; Squirrel prover integration
-(with-eval-after-load 'proof-general
+(with-eval-after-load 'proof-site
+  (message "========== LOADING SQUIRREL MODE ==========")
+  (require 'squirrel)
   (require 'ansi-color)
-  (require 'squirrel))
+  (message "========== SQUIRREL MODE LOADED =========="))
+(message "========== CUSTOM INIT.EL DONE ==========")
---
+++ packages/proof-general-with-squirrel/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  pkgs,
+  squirrel-prover-src,
+  squirrel,
+  ...
+}:
+pkgs.emacsPackages.trivialBuild {
+  pname = "proof-general-with-squirrel";
+  version = "unstable";
+  packageRequires = [ squirrel ] ++ pkgs.emacsPackages.proof-general.packageRequires;
+  buildCommand = ''
+    # Find the proof-general elpa directory name dynamically
+    PG_DIR=$(find ${pkgs.emacsPackages.proof-general}/share/emacs/site-lisp/elpa -type d -name "proof-general-*" -printf "%f\n" 2>/dev/null | head -1)
+
+    # Create output directory structure
+    mkdir -p $out/share/emacs/site-lisp/elpa
+
+    # Copy the original proof-general package
+    cp -r ${pkgs.emacsPackages.proof-general}/share/* $out/share/
+
+    # Make copied files writable so we can modify them
+    chmod -R u+w $out/share
+
+    # Add squirrel directory
+    mkdir -p $out/share/emacs/site-lisp/elpa/$PG_DIR/squirrel
+    if [ -d ${squirrel-prover-src}/utils ]; then
+      cp ${squirrel-prover-src}/utils/squirrel.el $out/share/emacs/site-lisp/elpa/$PG_DIR/squirrel/
+      cp ${squirrel-prover-src}/utils/squirrel-syntax.el $out/share/emacs/site-lisp/elpa/$PG_DIR/squirrel/
+    fi
+
+    # Update proof-site.el to register squirrel
+    if [ -f $out/share/emacs/site-lisp/elpa/$PG_DIR/generic/proof-site.el ]; then
+      sed -i '/^;; Entries in proof-assistant-table-default are lists of the form$/i \
+    (squirrel "Squirrel" "sp")' $out/share/emacs/site-lisp/elpa/$PG_DIR/generic/proof-site.el 2>/dev/null || true
+
+      # Remove compiled version so it gets recompiled
+      find . -name "*.elc" -exec rm {} \;
+      # rm -f $out/share/emacs/site-lisp/elpa/$PG_DIR/generic/proof-site.elc
+    fi
+  '';
+}
