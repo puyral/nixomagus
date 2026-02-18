c7653cf95c0e629bad616a25ae2a98651882a0a6
puyral <7871851+puyral@users.noreply.github.com>
2026-02-18 15:37:13 +0100

    rebuild Wed Feb 18 03:37:13 PM CET 2026

--- modules/home-manager/emacs/init.el
+++ modules/home-manager/emacs/init.el
@@ -1,7 +1,10 @@
+;; Register Squirrel with Proof General before loading proof-site
+(add-to-list 'proof-assistant-table-default '(squirrel "Squirrel" "sp"))
+
 ;; Load ProofGeneral
 (require 'proof-site)
 
-;; Squirrel prover integration (defered until after proof-general is loaded)
+;; Squirrel prover integration
 (with-eval-after-load 'proof-site
   (require 'squirrel)
   (require 'ansi-color))
