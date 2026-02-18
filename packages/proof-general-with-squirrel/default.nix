0330eaa81659c4faec00ad6c403ddb2703b38b0a
puyral <7871851+puyral@users.noreply.github.com>
2026-02-18 16:46:13 +0100

    rebuild Wed Feb 18 04:46:13 PM CET 2026

--- packages/proof-general-with-squirrel/default.nix
+++ packages/proof-general-with-squirrel/default.nix
@@ -4,27 +4,37 @@
   squirrel-prover-src,
   ...
 }:
-let
-  proof-general = pkgs.emacsPackages.proof-general;
-in
-proof-general.overrideAttrs (oldAttrs: {
+pkgs.emacsPackages.trivialBuild {
+  pname = "proof-general-with-squirrel";
+  version = "unstable";
+  packageRequires = [];
   buildCommand = ''
-    ${oldAttrs.buildCommand or ""}
+    # Find the proof-general elpa directory name dynamically
+    PG_DIR=$(find ${pkgs.emacsPackages.proof-general}/share/emacs/site-lisp/elpa -type d -name "proof-general-*" -printf "%f\n" 2>/dev/null | head -1)
+
+    # Create output directory structure
+    mkdir -p $out/share/emacs/site-lisp/elpa
+
+    # Copy the original proof-general package
+    cp -r ${pkgs.emacsPackages.proof-general}/share/* $out/share/
+
+    # Make copied files writable so we can modify them
+    chmod -R u+w $out/share
 
     # Add squirrel directory
-    mkdir -p $out/share/emacs/site-lisp/elpa/proof-general-20250915.1038/squirrel
+    mkdir -p $out/share/emacs/site-lisp/elpa/$PG_DIR/squirrel
     if [ -d ${squirrel-prover-src}/utils ]; then
-      cp ${squirrel-prover-src}/utils/squirrel.el $out/share/emacs/site-lisp/elpa/proof-general-20250915.1038/squirrel/
-      cp ${squirrel-prover-src}/utils/squirrel-syntax.el $out/share/emacs/site-lisp/elpa/proof-general-20250915.1038/squirrel/
+      cp ${squirrel-prover-src}/utils/squirrel.el $out/share/emacs/site-lisp/elpa/$PG_DIR/squirrel/
+      cp ${squirrel-prover-src}/utils/squirrel-syntax.el $out/share/emacs/site-lisp/elpa/$PG_DIR/squirrel/
     fi
 
     # Update proof-site.el to register squirrel
-    if [ -f $out/share/emacs/site-lisp/elpa/proof-general-20250915.1038/generic/proof-site.el ]; then
+    if [ -f $out/share/emacs/site-lisp/elpa/$PG_DIR/generic/proof-site.el ]; then
       sed -i '/^;; Entries in proof-assistant-table-default are lists of the form$/i \
-    (squirrel "Squirrel" "sp")' $out/share/emacs/site-lisp/elpa/proof-general-20250915.1038/generic/proof-site.el 2>/dev/null || true
+    (squirrel "Squirrel" "sp")' $out/share/emacs/site-lisp/elpa/$PG_DIR/generic/proof-site.el 2>/dev/null || true
 
       # Remove compiled version so it gets recompiled
-      rm -f $out/share/emacs/site-lisp/elpa/proof-general-20250915.1038/generic/proof-site.elc
+      rm -f $out/share/emacs/site-lisp/elpa/$PG_DIR/generic/proof-site.elc
     fi
   '';
-})
+}
