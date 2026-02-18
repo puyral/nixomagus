ee9e7f4e6934278ae070dc6965d08dbfd4a66330
puyral <7871851+puyral@users.noreply.github.com>
2026-02-18 17:52:18 +0100

    rebuild Wed Feb 18 05:52:18 PM CET 2026

--- modules/home-manager/emacs/init.el
+++ modules/home-manager/emacs/init.el
@@ -1,21 +1,3 @@
 (message "========== LOADING CUSTOM INIT.EL ==========")
-
-;; Define required variables for squirrel mode before loading proof-site
-;; (defvar squirrel-prog-args nil "Arguments for squirrel prover")
-;; (defvar squirrel-toolbar-entries nil "Toolbar entries for squirrel mode")
-
-;; Register Squirrel with Proof General before loading proof-site
-;; (add-to-list 'proof-assistant-table-default '(squirrel "Squirrel" "sp"))
-
-;; Load ProofGeneral
-;; (require 'proof-site)
-
-(setq debug-on-error t)
-
-;; Squirrel prover integration
-(with-eval-after-load 'proof-site
-  (message "========== LOADING SQUIRREL MODE ==========")
-  (require 'squirrel)
-  (require 'ansi-color)
-  (message "========== SQUIRREL MODE LOADED =========="))
+(provide 'squirrel)
 (message "========== CUSTOM INIT.EL DONE ==========")
