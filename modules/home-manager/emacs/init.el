f632a8cb00aedd9ef38f6f15528afb569516b2f5
puyral <7871851+puyral@users.noreply.github.com>
2026-02-18 13:16:30 +0100

    rebuild Wed Feb 18 01:16:30 PM CET 2026

--- modules/home-manager/emacs/init.el
+++ modules/home-manager/emacs/init.el
@@ -3,5 +3,12 @@
 
 ;; Squirrel prover integration
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/PG/squirrel"))
+
+;; Add squirrel to proof-assistant-table
+(setq proof-assistant-table
+      (cons '(squirrel "Squirrel" "sp")
+            proof-assistant-table))
+
+(require 'proof-easy-config)
 (require 'squirrel-syntax)
 (require 'squirrel)
