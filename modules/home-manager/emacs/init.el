58d7e4405ed3cd589f23a987476e76c468c4554a
puyral <7871851+puyral@users.noreply.github.com>
2026-02-19 09:09:16 +0100

    rebuild Thu Feb 19 09:09:16 AM CET 2026

--- modules/home-manager/emacs/init.el
+++ modules/home-manager/emacs/init.el
@@ -1,5 +1,14 @@
 (message "========== LOADING CUSTOM INIT.EL ==========")
+;; Squirrel
 (provide 'squirrel)
 (require 'ansi-color)
+
+;; mouse in TUI
 (xterm-mouse-mode 1)
+
+
+;; Enable Evil
+;; (require 'evil)
+(evil-mode 1)
+
 (message "========== CUSTOM INIT.EL DONE ==========")
