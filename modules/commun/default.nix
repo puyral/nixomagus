f0408c256a156891410cfbcdad4896b09ea64f62
puyral <7871851+puyral@users.noreply.github.com>
2026-02-21 12:44:55 +0100

    .

--- modules/commun/default.nix
+++ modules/commun/default.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   modules.nixos = [ "tailscale_ips" ];
+  modules.homeManager = [ "tailscale_ips" ];
   flake = {
     nixosModules.tailscale_ips = ./tailscale;
     homeModules.tailscale_ips = ./tailscale;
