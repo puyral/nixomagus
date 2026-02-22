acedc3ee1646ad096fde98a99264e1e847519b0f
puyral <7871851+puyral@users.noreply.github.com>
2026-02-22 18:27:35 +0100

    rebuild Sun Feb 22 06:27:35 PM CET 2026

--- modules/home-manager/anyrun/default.nix
+++ modules/home-manager/anyrun/default.nix
@@ -1,6 +1,9 @@
 { config, lib, ... }:
 let
   cfg = config.extra.anyrun;
+  pkg = config.programs.anyrun.package;
+  mkPlugin = p: "${pkg}/lib/lib${p}.so";
+  mkPlugins = lib.map mkPlugin;
 in
 {
   options.extra.anyrun = {
@@ -12,6 +15,39 @@ in
       config = {
         closeOnClick = true;
         hidePluginInfo = true;
+        x = {
+          fraction = 0.5;
+        };
+        y = {
+          fraction = 0.3;
+        };
+        width = {
+          fraction = 0.3;
+        };
+        hideIcons = false;
+
+        plugins = (
+          mkPlugins [
+            "applications"
+            "nix_run"
+            "rink"
+            "shell"
+          ]
+        );
+      };
+      extraConfigFiles = {
+        "nix-run.ron".text = ''
+          '
+          // <Anyrun config dir>/nix-run.ron
+          Config(
+            prefix: ":nr ",
+            // Whether or not to allow unfree packages
+            allow_unfree: false,
+            // Nixpkgs channel to get the package list from
+            channel: "nixpkgs-unstable",
+            max_entries: 3,
+          )
+        '';
       };
     };
   };
