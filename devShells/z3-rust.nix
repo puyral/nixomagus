25f36d3e15445429fbd7b02798a8d9699ec5f5ed
Puyral <simon.jeanteur@tuwien.ac.at>
2026-02-24 14:36:57 +0100

    better nix shell for z3.rs

--- devShells/z3-rust.nix
+++ devShells/z3-rust.nix
@@ -6,15 +6,28 @@
   devShells.z3-rust =
 
     pkgs.mkShell {
-      name = "config";
+      name = "z3.rs";
       RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+
+      # Tools needed at build time
+      nativeBuildInputs = with pkgs; [
+        cmake
+        python3
+        ninja
+        rustPlatform.bindgenHook
+      ];
+
       buildInputs = with pkgs; [
         cargo
         clippy
         cargo-expand
         rust-analyzer
-        rustPlatform.bindgenHook
         z3
       ];
+
+      # Force the cmake crate to use Ninja for a much faster Z3 build
+      shellHook = ''
+        export CMAKE_GENERATOR="Ninja"
+      '';
     };
 }
