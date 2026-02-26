c40c31abb35b7973eb5e60226be1c5467ed24079
puyral <7871851+puyral@users.noreply.github.com>
2026-02-26 17:51:31 +0100

    nix fmt

--- modules/nixos/gui/mangowc.nix
+++ modules/nixos/gui/mangowc.nix
@@ -17,25 +17,25 @@ in
     programs.mango = {
       enable = true;
     };
-    extra.gui.extraWlrInUse = ["mango"];
+    extra.gui.extraWlrInUse = [ "mango" ];
 
     xdg.portal = {
       config.mango = lib.mkForce {
-          default = [
-            "gtk"
-          ];
-          # except those
-          "org.freedesktop.impl.portal.Secret" = ["gnome-keyring"];
-          "org.freedesktop.impl.portal.ScreenCast" = ["luminous"];
-          "org.freedesktop.impl.portal.ScreenShot" = ["luminous"];
+        default = [
+          "gtk"
+        ];
+        # except those
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+        "org.freedesktop.impl.portal.ScreenCast" = [ "luminous" ];
+        "org.freedesktop.impl.portal.ScreenShot" = [ "luminous" ];
 
-          "org.freedesktop.impl.portal.RemoteDesktop" = ["luminous"];
-          "org.freedesktop.impl.portal.InputCapture" = ["luminous"];
-          "org.freedesktop.impl.portal.Settings" = ["luminous"];
+        "org.freedesktop.impl.portal.RemoteDesktop" = [ "luminous" ];
+        "org.freedesktop.impl.portal.InputCapture" = [ "luminous" ];
+        "org.freedesktop.impl.portal.Settings" = [ "luminous" ];
 
-          # wlr does not have this interface
-          "org.freedesktop.impl.portal.Inhibit" = [];
-        };
+        # wlr does not have this interface
+        "org.freedesktop.impl.portal.Inhibit" = [ ];
+      };
       extraPortals = with pkgs; [
         xdg-desktop-portal-luminous
       ];
