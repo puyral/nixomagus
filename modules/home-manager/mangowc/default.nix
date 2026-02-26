8bf432095cd60c3cde8ec32788efcb90e983d811
puyral <7871851+puyral@users.noreply.github.com>
2026-02-26 11:05:33 +0100

    rebuild Thu Feb 26 11:05:33 AM CET 2026

--- modules/home-manager/mangowc/default.nix
+++ modules/home-manager/mangowc/default.nix
@@ -33,10 +33,36 @@ in
     };
     wayland.windowManager.mango = {
       enable = true;
+      systemd = {
+        enable = true;
+        xdgAutostart = true;
+      };
     };
     home.packages = with pkgs; [
       foot
       rofi
     ];
+    # home.xdg.portal = {
+    #   enable = true;
+    #   # xdgOpenUsePortal = true;
+    #   config = {
+    #     mango = {
+    #       default = [
+    #         "gtk"
+    #       ];
+    #       # except those
+    #       "org.freedesktop.impl.portal.Secret" = ["gnome-keyring"];
+    #       "org.freedesktop.impl.portal.ScreenCast" = ["wlr"];
+    #       "org.freedesktop.impl.portal.ScreenShot" = ["wlr"];
+
+    #       # wlr does not have this interface
+    #       "org.freedesktop.impl.portal.Inhibit" = [];
+    #     };
+    #   };
+    #   extraPortals = with pkgs; [
+    #     xdg-desktop-portal-wlr
+    #     xdg-desktop-portal-gtk
+    #   ];
+    # };
   };
 }
