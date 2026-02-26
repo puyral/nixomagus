60541771d03d0ff635ac5e2e266975df593b3de2
puyral <7871851+puyral@users.noreply.github.com>
2026-02-26 17:51:26 +0100

    ntfy to desktop

--- packages/notify-done/ntfy-done.sh
+++ packages/notify-done/ntfy-done.sh
@@ -46,23 +46,6 @@ else
   # Command mode
   CMD="$*"
   set +e # Don't exit on command failure
-  # Run command, capture stdout/stderr to temp file and stdout
-  # We use a subshell to pipe, but we need the exit code.
-  # PIPESTATUS tells us the exit code of the first command in the pipe.
-  # "$@" 2>&1 | tee "$TF"
-  # RET=$?
-  # # shellcheck disable=SC2181
-  # if [ $? -ne 0 ]; then
-  #      # This checks tee's exit code, which is usually 0.
-  #      # In bash, PIPESTATUS[0] would work, but writeShellApplication uses bash (usually).
-  #      # Let's trust set -o pipefail if we could use it, but strictly:
-  #      :
-  # fi
-  # Better approach for bash:
-  # ( set -o pipefail; "$@" 2>&1 | tee "$TF" )
-  # But capturing the exit code of the command inside the subshell is hard to propagate cleanly without another file.
-  
-  # Alternative: run command, redirect to file, then cat file? No, not tee-like (realtime).
   
   # Using pipefail:
   set -o pipefail
