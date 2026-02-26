6227197a63156f3d689a91d0f1661791816c237b
puyral <7871851+puyral@users.noreply.github.com>
2026-02-26 08:24:28 +0100

    rebuild Thu Feb 26 08:24:28 AM CET 2026

--- devShells/z3-rust.nix
+++ devShells/z3-rust.nix
@@ -6,15 +6,29 @@
   devShells.z3-rust =
 
     pkgs.mkShell {
-      name = "config";
+      name = "z3.rs";
       RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+
+      # Tools needed at build time
+      nativeBuildInputs = with pkgs; [
+        cmake
+        python3
+        ninja
+        rustPlatform.bindgenHook
+      ];
+
       buildInputs = with pkgs; [
         cargo
         clippy
         cargo-expand
         rust-analyzer
-        rustPlatform.bindgenHook
         z3
+        gdb
       ];
+
+      # Force the cmake crate to use Ninja for a much faster Z3 build
+      #      shellHook = ''
+      #        export CMAKE_GENERATOR="Ninja"
+      #      '';
     };
 }
