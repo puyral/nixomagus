56c9b47b09e343144df1a4dde6352edcc895fa16
puyral (dynas) <github@puyral.fr>
2026-03-01 08:52:38 +0100

    rebuild Sun Mar  1 08:52:38 AM CET 2026

--- modules/home-manager/emacs/options.nix
+++ modules/home-manager/emacs/options.nix
@@ -1,7 +1,13 @@
-{ lib, ... }:
+{ lib, config, ... }:
 {
   options.extra.emacs = with lib; {
     enable = mkEnableOption "emacs";
+
+    nox = mkOption {
+      type = types.bool;
+      default = config.extra.gui.enable;
+      description = "wethere to add X support for emacs.";
+    };
 
     extensions = mkOption {
       type = types.listOf types.package;
