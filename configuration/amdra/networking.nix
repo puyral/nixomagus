ec6559acc0221189c993da5db63e02e85be9a16a
puyral (dynas) <github@puyral.fr>
2026-03-02 20:09:14 +0100

    rebuild Mon Mar  2 08:09:14 PM CET 2026

--- configuration/amdra/networking.nix
+++ configuration/amdra/networking.nix
@@ -1,26 +1,35 @@
-{ ... }:
+{ config, ... }:
 {
   services.cloudflare-warp.enable = true;
-  networking.networkmanager.unmanaged = [ "interface-name:enp10s0" ];
+  networking.networkmanager.unmanaged = [ "interface-name:enp15s0" ];
   networking.interfaces = {
     enp10s0 = {
       wakeOnLan.enable = true;
-      useDHCP = false;
-      ipv4.addresses = [ ];
-      ipv6.addresses = [ ];
+      useDHCP = true;
     };
     enp15s0 = {
       wakeOnLan.enable = true;
-      useDHCP = true;
-      # mtu = 9000;
-      # ipv4.addresses = [
-      #   {
-      #     address = "192.168.1.3";
-      #     prefixLength = 24;
-      #   }
-      # ];
-      # ipv6.addresses = [];
+      useDHCP = false;
+      mtu = 9000;
+      ipv4.addresses = [
+        {
+          address = "192.168.100.1";
+          prefixLength = 24;
+        }
+      ];
     };
   };
   systemd.services.NetworkManager-wait-online.enable = false;
+
+  networking.nat = {
+    externalInterface = config.vars.mainNetworkInterface;
+
+    # for backup if needed
+    # externalInterface = "wlp8s0"; # wifi
+  };
+
+  vars = {
+    mainNetworkInterface = "enp10s0";
+    fastIp = "192.168.100.1";
+  };
 }
