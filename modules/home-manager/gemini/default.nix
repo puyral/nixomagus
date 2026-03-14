81488d41a9b176ec8ba45bd5499989c05fef06da
puyral (dynas) <github@puyral.fr>
2026-03-14 17:43:31 +0100

    rebuild Sat Mar 14 05:43:31 PM CET 2026

---
+++ modules/home-manager/gemini/default.nix
@@ -0,0 +1,37 @@
+{lib, config, pkgs-self, pkgs-unstable, ...}:
+let cfg = config.extra.gemini;
+in
+{
+  options.extra.gemini = {
+    enable = lib.mkEnableOption "gemini";
+    lean = lib.mkEnableOption "lean mcp";
+  };
+  config = lib.mkIf cfg.enable {
+    programs.gemini-cli = {
+      enable = true;
+      package = pkgs-unstable.gemini-cli;
+      settings = {
+        general = {
+          preview = true;
+        };
+        model = {
+          name = "auto";
+        };
+        security = {
+          auth = {
+            selectedType = "oauth-personal";
+          };
+        };
+        mcp = {
+          allowed = ["lean"];
+        };
+        mcpServers = {
+          lean = {
+            command = "${pkgs-self.lean-lsp-mcp}/bin/lean-lsp-mcp";
+            trust = true;
+          };
+        };
+      };
+    };
+  };
+}
