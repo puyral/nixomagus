34c7b5b58aff1f70cfb199eb6f75b0704dd0349d
puyral (dynas) <github@puyral.fr>
2026-03-26 08:24:09 +0100

    rebuild Thu Mar 26 08:24:09 AM CET 2026

--- modules/nixos/esphome/default.nix
+++ modules/nixos/esphome/default.nix
@@ -1,22 +1,28 @@
-{config, lib, pkgs, ...}:
-let cfg = config.extra.esphome;
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.extra.esphome;
   # mqtt_port = (builtins.head config.services.mosquitto.listeners).port;
   # mqtt = "mqtt://localhost:${builtins.toString mqtt_port}";
 
- in
+in
 {
-options.extra.esphome = with lib; {
-  enable = mkEnableOption "esphome";
-  address = mkOption {
-    type = types.str;
+  options.extra.esphome = with lib; {
+    enable = mkEnableOption "esphome";
+    address = mkOption {
+      type = types.str;
+    };
   };
-};
 
-
-config = lib.mkIf cfg.enable {
-  services.esphome = {
-    enable = true;
-    address = cfg.address;
-    openFirewall = true;
-};};
+  config = lib.mkIf cfg.enable {
+    services.esphome = {
+      enable = true;
+      address = cfg.address;
+      openFirewall = true;
+    };
+  };
 }
