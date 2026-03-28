8fc7ed299bc725083ab8325e84a4b5ee4f8d5c97
puyral <github@puyral.fr>
2026-03-28 16:56:40 +0100

    rebuild Sat Mar 28 04:56:40 PM CET 2026

--- modules/nixos/jellyfin/default.nix
+++ modules/nixos/jellyfin/default.nix
@@ -1,6 +1,5 @@
 {
   config,
-  pkgs,
   lib,
   computer,
   ...
@@ -13,10 +12,6 @@ in
   imports = [ ./options.nix ];
 
   config = lib.mkIf cfg.enable (
-    let
-      localAddress = config.containers.${name}.localAddress;
-      entrypoint = name;
-    in
     {
       containers.${name} = {
         bindMounts = {
@@ -32,11 +27,6 @@ in
             hostPath = cfg.videoDir;
             isReadOnly = false;
           };
-          # gpu - existing jellyfin config had all of /dev/dri
-          # "/dev/dri" = {
-          #   hostPath = "/dev/dri";
-          #   isReadOnly = false;
-          # };
         };
       };
 
@@ -48,7 +38,7 @@ in
 
       # The container configuration itself
       containers.${name}.config =
-        { lib, pkgs, ... }:
+        {...}:
         {
           services.jellyfin = {
             enable = true;
@@ -57,14 +47,8 @@ in
           };
           services.jellyseerr.enable = true;
 
-          system.stateVersion = computer.stateVersion;
-          networking = {
-            firewall.enable = true;
-            useHostResolvConf = lib.mkForce false;
-          };
 
           programs.nix-ld.enable = true;
-          services.resolved.enable = true;
           # We need to make sure these users/groups exist or are inherited
           # In the original, they were assigned from config.users.users.jellyfin
           # which refers to the HOST users.
@@ -74,15 +58,6 @@ in
           users.groups."video".gid = config.users.groups."video".gid;
 
           systemd.services.jellyfin.serviceConfig.DeviceAllow = [ "/dev/dri/renderD128" ];
-
-          hardware.graphics = {
-            enable = true;
-            extraPackages = with pkgs; [
-              intel-media-driver
-              vpl-gpu-rt
-              intel-compute-runtime
-            ];
-          };
         };
 
       users.users.jellyfin = {
@@ -97,63 +72,6 @@ in
       extra.extraGroups.jellyfin = {
         gid = 1100;
       };
-
-      # Manual Traefik configuration as it was in the original
-      # services.traefik.staticConfigOptions.entryPoints.${entrypoint} = ":9999";
-
-      # services.traefik.dynamicConfigOptions.http = {
-      #   routers.${name} = {
-      #     rule = "Host(`${name}.puyral.fr`)";
-      #     entrypoints = entrypoint;
-      #     tls = {
-      #       certResolver = "ovh";
-      #       domains = "${name}.puyral.fr";
-      #     };
-      #     middlewares = "${name}";
-      #     service = "${name}";
-      #   };
-      #   middlewares.${name} = {
-      #     headers = {
-      #       customResponseHeaders.X-Robots-Tag = [
-      #         "noindex"
-      #         "nofollow"
-      #         "nosnippet"
-      #         "noarchive"
-      #         "notranslate"
-      #         "noimageindex"
-      #       ];
-      #       SSLRedirect = true;
-      #       SSLHost = "${name}.puyral.fr:9999";
-      #       SSLForceHost = true;
-      #       STSSeconds = 315360000;
-      #       STSIncludeSubdomains = true;
-      #       STSPreload = true;
-      #       forceSTSHeader = true;
-      #       frameDeny = true;
-      #       contentTypeNosniff = true;
-      #       customresponseheaders.X-XSS-PROTECTION = 1;
-      #       customFrameOptionsValue = "allow-from https://puyral.fr";
-      #     };
-      #   };
-      #   services.${name} = {
-      #     loadBalancer = {
-      #       servers = [ { url = "http://${localAddress}:8096"; } ];
-      #       passHostHeader = true;
-      #     };
-      #   };
-
-      #   routers."${name}-insecure" = {
-      #     rule = config.services.traefik.dynamicConfigOptions.http.routers.${name}.rule;
-      #     entrypoints = "${entrypoint}";
-      #     middlewares = "${name}-insecure";
-      #     service = "noop@internal";
-      #   };
-      #   middlewares."${name}-insecure".redirectscheme = {
-      #     scheme = "https";
-      #     port = 9999;
-      #     permanent = false;
-      #   };
-      # };
 
       networking.firewall.allowedTCPPorts = [
         9999
