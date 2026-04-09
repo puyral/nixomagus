bc2e66b7374c84106d79bc197e424b1342ec714c
puyral <github@puyral.fr>
2026-04-09 20:51:55 +0200

    rebuild Thu Apr  9 08:51:55 PM CEST 2026

--- users/simon/sandbox/default.nix
+++ users/simon/sandbox/default.nix
@@ -2,5 +2,10 @@
 {
   # Minimal home-manager config for the sandbox
   # imports = [ ./commun ]; # already imported by users/simon/default.nix
-  extra.jail.enable = true;
+  extra ={jail.enable = true;
+
+      opencode = {
+      enable = true;
+      leanSupport.mcp = true;
+    };};
 }
