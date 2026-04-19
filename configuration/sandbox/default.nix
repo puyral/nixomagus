60a7815befa8833253cd3f5b84971b60e86e4d0f
puyral (dynas) <github@puyral.fr>
2026-04-19 12:46:46 +0200

    more vm

--- configuration/sandbox/default.nix
+++ configuration/sandbox/default.nix
@@ -14,12 +14,26 @@
   # Disable things from commun that we don't need or want
   extra.tailscale.enable = lib.mkForce false;
   extra.mail.enable = lib.mkForce false;
+
+  # because libgit2 is broken in VM, I use lix
+  # see https://lix.systems/add-to-config/
+  nixpkgs.overlays = [
+    (final: prev: {
+      inherit (prev.lixPackageSets.stable)
+        nixpkgs-review
+        nix-eval-jobs
+        nix-fast-build
+        colmena
+        ;
+    })
+  ];
+  nix.package = pkgs.lixPackageSets.stable.lix;
+
   programs.gnupg.agent.enable = lib.mkForce false;
 
   # Network configuration for the user-mode interface
   networking.useNetworkd = true;
   networking.networkmanager.enable = lib.mkForce false;
-  networking.firewall.allowedTCPPorts = [ 22 ];
 
   nix = {
     enable = true;
@@ -29,20 +43,20 @@
   services.openssh = {
     enable = true;
     settings.PermitRootLogin = "yes";
+    openFirewall = true;
   };
-
-  services.getty.autologinUser = "simon";
 
   # We want a minimal system
   environment.systemPackages = with pkgs; [
-    # gitMinimal
     vim
     htop
-    bindfs
   ];
+
+  zramSwap.enable = true;
 
   # Ensure simon has the right groups
   users.users.simon.extraGroups = [ "wheel" ];
+  services.getty.autologinUser = "simon";
 
   # Allow root login without password for convenience in the sandbox
   # users.users.root.initialPassword = "";
