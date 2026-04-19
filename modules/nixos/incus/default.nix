700191af722dbcb11bfcc1be696093999c2bd139
puyral (dynas) <github@puyral.fr>
2026-04-19 09:42:53 +0200

    rebuild Sun Apr 19 09:42:53 AM CEST 2026

--- modules/nixos/incus/default.nix
+++ modules/nixos/incus/default.nix
@@ -23,8 +23,53 @@ let cfg = config.extra.incus; in
 
   # https://wiki.nixos.org/wiki/Incus#Networking/Firewall
   networking.nftables.enable = true;
+  networking.firewall.interfaces.incusbr0.allowedTCPPorts = [
+  53
+  67
+];
+networking.firewall.interfaces.incusbr0.allowedUDPPorts = [
+  53
+  67
+];
 
   users.groups.incus.members = ["root"] ++ cfg.users;
   users.groups.incus-admin.members = ["root"] ++ cfg.admins;
  };
+
+
+
+#  config: {}
+# networks:
+# - config:
+#     ipv4.address: auto
+#     ipv6.address: auto
+#   description: ""
+#   name: incusbr0
+#   type: ""
+#   project: default
+# storage_pools:
+# - config:
+#     source: Zeno/containers/incus
+#   description: ""
+#   name: zfs
+#   driver: zfs
+# storage_volumes: []
+# profiles:
+# - config: {}
+#   description: ""
+#   devices:
+#     eth0:
+#       name: eth0
+#       network: incusbr0
+#       type: nic
+#     root:
+#       path: /
+#       pool: zfs
+#       type: disk
+#   name: default
+#   project: default
+# projects: []
+# certificates: []
+# cluster_groups: []
+# cluster: null
 }
