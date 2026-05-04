1926426800c63465506f59f43bac9466cca17f47
puyral (dynas) <github@puyral.fr>
2026-05-04 10:17:16 +0200

    rebuild Mon May  4 10:17:16 AM CEST 2026

--- GEMINI.md
+++
@@ -1,5 +0,0 @@
-You are a `nix` expert.
-
-The config should always build with `nix build .#nixosConfigurations."$HOST".config.system.build.toplevel` when relevant and be checked by `nix flake check .#`. The latter command can be slow, so it should only considered as a last step
-
-`git` uses gpg by default which requires a password. Therefore tell git to not sign when you commit
--- users/default.nix
+++ users/default.nix
@@ -15,14 +15,4 @@
   };
 
   # nix.registry = (import ./registeries.nix) attrs;
-
-  # better caching
-  # nix.settings.substituters = [
-  #   "https://aseipp-nix-cache.global.ssl.fastly.net"
-  #   "https://cache.nixos.org/"
-  #   "https://nix-community.cachix.org"
-  # ];
-  # nix.settings.trusted-public-keys = [
-  #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  # ];
 }
