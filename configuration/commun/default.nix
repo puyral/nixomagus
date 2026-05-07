f68962dfdf228241c8734e36c6b49a88c5992e07
puyral (dynas) <github@puyral.fr>
2026-05-08 01:08:22 +0200

    reformat

--- configuration/commun/default.nix
+++ configuration/commun/default.nix
@@ -29,8 +29,6 @@ in
     agent = {
       enable = true;
       enableSSHSupport = true;
-      # pinentryPackage = pkgs.pinentry-gnome3;
-      # pinentryFlavor = "gnome3";
     };
   };
   # Use the systemd-boot EFI boot loader.
@@ -45,48 +43,13 @@ in
     # Pick only one of the below networking options.
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
     networkmanager = {
-      enable = true; # Easiest to use and most distros use this by default.
-
-      # insertNameservers = [ "1.1.1.1" ];
+      enable = lib.mkDefault true; # Easiest to use and most distros use this by default.
     };
   };
 
-  # Set your time zone.
   time.timeZone = "Europe/Vienna";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
   programs.zsh.enable = true;
-
-  # for compatibility
   extra.nix-ld.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # home-manager.useGlobalPkgs = true; # use global pkgs
-  # Define a user account. Don't forget to set a password with ‘passwd’.
 
   users = {
     groups.nix-config = {
@@ -109,14 +72,11 @@ in
   };
   security.sudo.wheelNeedsPassword = false;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     htop
     gitFull
-    # tmux
     usbutils
     pciutils
   ];
@@ -127,12 +87,6 @@ in
 
   # garbage collection
   extra.cache.substituter = lib.mkDefault false; # dynas is down
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-
-  # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh = {
@@ -154,15 +108,12 @@ in
       dates = [ "03:45" ];
     };
 
-    # extraOptions = ''
-    #   builders-use-substitutes = true
-    # '';
     settings.trusted-users = [
       "root"
       "simon"
       "@wheel"
     ];
-    buildMachines = [
+    buildMachines = if true then [] else [
       # vampire
       {
         hostName = "root@100.64.0.16";
@@ -212,17 +163,6 @@ in
       allowUnfree = true;
     };
   };
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-  # networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
