13f8aa4eb2ce21203bac9a17439ac3901615b67d
puyral <github@puyral.fr>
2026-06-10 17:22:38 +0200

    re-introduce lean-lsp

--- packages/default.nix
+++ packages/default.nix
@@ -39,7 +39,8 @@
         {
           sops-nix = sops-nix.packages.default;
           darktable-jpeg-sync = darktable-jpeg-sync.packages.default;
-        };
+        }
+        // lean-lsp-mcp.packages;
     in
     {
 
