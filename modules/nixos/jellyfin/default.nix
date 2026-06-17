bb4028d6733c35655d9d8ba900455132e645672d
puyral (dynas) <github@puyral.fr>
2026-06-17 19:45:37 +0200

    fmt

--- modules/nixos/jellyfin/default.nix
+++ modules/nixos/jellyfin/default.nix
@@ -70,7 +70,10 @@ in
             add_header Permissions-Policy "accelerometer=(), ambient-light-sensor=(), battery=(), bluetooth=(), camera=(), clipboard-read=(), display-capture=(), document-domain=(), encrypted-media=(), gamepad=(), geolocation=(), gyroscope=(), hid=(), idle-detection=(), interest-cohort=(), keyboard-map=(), local-fonts=(), magnetometer=(), microphone=(), payment=(), publickey-credentials-get=(), serial=(), sync-xhr=(), usb=(), xr-spatial-tracking=()" always;
             add_header Content-Security-Policy "default-src https: data: blob: ; img-src 'self' https://* ; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' https://www.gstatic.com https://www.youtube.com blob:; worker-src 'self' blob:; connect-src 'self'; object-src 'none'; font-src 'self'";
           '';
-	  providers = [ "dynas" "ovh-pl" ];
+          providers = [
+            "dynas"
+            "ovh-pl"
+          ];
         }
         {
           enable = true;
@@ -78,7 +81,10 @@ in
           name = "jellyfin-socket";
           subdomain = "jellyfin";
           path = "/socket";
-	  providers = [ "dynas" "ovh-pl" ];
+          providers = [
+            "dynas"
+            "ovh-pl"
+          ];
         }
         # {
         #   enable = true;
