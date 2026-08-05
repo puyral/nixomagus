3ca0f086a4ffef0cbd05de8e092079ee181ea75c
puyral <github@puyral.fr>
2026-08-05 13:08:01 +0200

    update hash

--- packages/mango/default.nix
+++ packages/mango/default.nix
@@ -3,7 +3,7 @@ inputs'.mango.packages.mango.overrideAttrs (old: {
   patches = (old.patches or [ ]) ++ [
     (pkgs.fetchpatch {
       url = "https://github.com/mangowm/mango/pull/1121.patch";
-      sha256 = "02bgb1v7qlizaj5xqy3f3cz6cp4nq1l8mbrqax26mcvjbpi7kc1l";
+      hash = "sha256-Cv81tvhM5jifQF13Fk6/UFL+9ThuV2ls2xkPtFGV02g=";
     })
   ];
 })
